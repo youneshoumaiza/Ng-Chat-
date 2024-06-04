0f56ab165ef1a5bbeab267b114b024fac235ecc8
Younes Houmaiza <youneshoumaiza10@gmail.com>
2024-06-04 12:38:46 +0100

    SOA project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:false,
+    supabaseUrl:'https://gzajiwdoszveaqcdiora.supabase.co',
+    supabaseKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6YWppd2Rvc3p2ZWFxY2Rpb3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5OTEyNDEsImV4cCI6MjAzMjU2NzI0MX0.HKILZn6ZfDlIh_-UObgYbgthS17K2JQ7Gvd_3TldPMA',
+};
